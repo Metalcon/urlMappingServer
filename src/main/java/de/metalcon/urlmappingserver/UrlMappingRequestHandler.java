5d4c2cd5741d29d9f506d9f5ecc512801600d84b
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2014-03-18 17:51:15 +0100

    zeroMQ integration started

---
+++ src/main/java/de/metalcon/urlmappingserver/UrlMappingRequestHandler.java
@@ -0,0 +1,29 @@
+package de.metalcon.urlmappingserver;
+
+import java.io.Serializable;
+
+import de.metalcon.urlmappingserver.api.requests.UrlMappingRegistrationRequest;
+
+public class UrlMappingRequestHandler implements ZMQRequestHandler {
+
+    private EntityUrlMappingManager urlMappingManager;
+
+    public UrlMappingRequestHandler(
+            EntityUrlMappingManager urlMappingManager) {
+        this.urlMappingManager = urlMappingManager;
+    }
+
+    @Override
+    public Serializable handleRequest(Object request) {
+        if (request instanceof UrlMappingRegistrationRequest) {
+            UrlMappingRegistrationRequest registrationRequest =
+                    (UrlMappingRegistrationRequest) request;
+            urlMappingManager.registerMuid(registrationRequest.getUrlData());
+
+            return "OK.";
+        }
+
+        return null;
+    }
+
+}
