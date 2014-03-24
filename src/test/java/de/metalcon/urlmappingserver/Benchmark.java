407bc80fed0adef07eb3d5e3bc65f1ac8d9d1b95
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2014-03-24 17:30:05 +0100

    local benchmark

--- src/test/java/de/metalcon/urlmappingserver/Benchmark.java
+++ src/test/java/de/metalcon/urlmappingserver/Benchmark.java
@@ -1,6 +1,10 @@
 package de.metalcon.urlmappingserver;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import org.zeromq.ZMQ;
 
@@ -33,27 +37,27 @@ public class Benchmark {
 
     protected ZMQ.Socket socket;
 
-    protected static short ID = 1;
+    protected static long ID = 1;
 
-    public static void main(String[] args) {
-        String address = ADDRESS_TCP;
+    //    public static void main(String[] args) {
+    //        String address = ADDRESS_TCP;
+    //
+    //        CONTEXT = ZMQ.context(1);
+    //        WORKER =
+    //                new ZMQWorker(address, new UrlMappingRequestHandler(
+    //                        new EntityUrlMappingManager()), CONTEXT);
+    //        WORKER.start();
+    //
+    //        Benchmark benchmark = new Benchmark(ADDRESS_TCP);
+    //        benchmark.start();
+    //    }
 
-        CONTEXT = ZMQ.context(1);
-        WORKER =
-                new ZMQWorker(address, new UrlMappingRequestHandler(
-                        new EntityUrlMappingManager()), CONTEXT);
-        WORKER.start();
-
-        Benchmark benchmark = new Benchmark(ADDRESS_TCP);
-        benchmark.start();
-    }
-
-    public Benchmark(
-            String address) {
-        this.address = address;
-        socket = CONTEXT.socket(ZMQ.DEALER);
-        socket.connect(address);
-    }
+    //    public Benchmark(
+    //            String address) {
+    //        this.address = address;
+    //        socket = CONTEXT.socket(ZMQ.DEALER);
+    //        socket.connect(address);
+    //    }
 
     public void start() {
 
@@ -88,15 +92,13 @@ public class Benchmark {
             case RECORD:
                 return new RecordUrlData(muid, name,
                         (BandUrlData) generatedData(MuidType.BAND
-                                .getRawIdentifier()), ID);
+                                .getRawIdentifier()), 1914 + (int) (ID % 100));
             case TOUR:
-                return new TourUrlData(muid, name, ID);
+                return new TourUrlData(muid, name, 1914 + (int) (ID % 100));
             case TRACK:
-                return new TrackUrlData(muid, name,
-                        (BandUrlData) generatedData(MuidType.BAND
-                                .getRawIdentifier()),
+                return new TrackUrlData(muid, name, null,
                         (RecordUrlData) generatedData(MuidType.RECORD
-                                .getRawIdentifier()), ID);
+                                .getRawIdentifier()), 1 + (int) (ID % 30));
             case USER:
                 return new UserUrlData(muid, String.valueOf(ID),
                         String.valueOf(ID));
@@ -106,7 +108,137 @@ public class Benchmark {
                                 .getRawIdentifier()));
         }
 
-        throw new UnsupportedOperationException("unknown entity type");
+        throw new UnsupportedOperationException("unknown MUID type");
     }
 
+    public static void main(String[] args) {
+        short type = 9;
+        EntityUrlMappingManager manager = new EntityUrlMappingManager();
+
+        long totalWrites = 1000000;
+        long crrNano = System.nanoTime();
+        List<EntityUrlData> registered = new LinkedList<EntityUrlData>();
+
+        System.out.println("progress:");
+        for (long write = 0; write < totalWrites; write++) {
+            EntityUrlData entity = generatedData(type);
+            registered.add(entity);
+            manager.registerMuid(entity);
+
+            type -= 1;
+            if (type == -1) {
+                type = 9;
+            }
+
+            if (write % (totalWrites / 10) == 0 && write != 0) {
+                System.out.println((write / (double) totalWrites * 100) + "%");
+            }
+        }
+        crrNano = System.nanoTime() - crrNano;
+        long crrMis = crrNano / 1000;
+        long crrMs = crrMis / 1000;
+        System.out.println("benchmark duration (write): " + crrMs + "ms");
+        System.out.println("per write: " + (crrMis / totalWrites) + "µs");
+        System.out.println("writes per second: " + 1000
+                / (crrMs / (double) totalWrites));
+
+        long totalReads = 10000000;
+        long read = 0;
+        crrNano = System.nanoTime();
+        Muid muid;
+        while (true) {
+            for (EntityUrlData entity : registered) {
+                if (read++ >= totalReads) {
+                    break;
+                }
+                muid =
+                        manager.resolveMuid(generateUrl(entity), entity
+                                .getMuid().getMuidType());
+                if (muid == null) {
+                    System.err.println("failed to resolve MUID ("
+                            + entity.getMuid().getMuidType() + ")");
+                    return;
+                }
+            }
+            if (read >= totalReads) {
+                break;
+            }
+        }
+        crrNano = System.nanoTime() - crrNano;
+        crrMis = crrNano / 1000;
+        crrMs = crrMis / 1000;
+        System.out.println("benchmark duration (read): " + crrMs + "ms");
+        System.out.println("per read: " + (crrNano / totalReads) + "ns");
+        System.out.println("reads per second: " + 1000
+                / (crrMs / (double) totalReads));
+    }
+
+    protected static Map<String, String> generateUrl(EntityUrlData entity) {
+        Map<String, String> pathVars = new HashMap<String, String>();
+
+        String mapping = entity.getName() + "-" + entity.getMuid();
+        switch (entity.getMuid().getMuidType()) {
+
+            case BAND:
+                pathVars.put("pathBand", mapping);
+                break;
+
+            case CITY:
+                pathVars.put("pathCity", mapping);
+                break;
+
+            case EVENT:
+                pathVars.put("pathEvent", mapping);
+                break;
+
+            case GENRE:
+                pathVars.put("pathGenre", mapping);
+                break;
+
+            case INSTRUMENT:
+                pathVars.put("pathInstrument", mapping);
+                break;
+
+            case RECORD:
+                BandUrlData band = ((RecordUrlData) entity).getBand();
+                if (!band.hasEmptyMuid()) {
+                    pathVars.put("pathBand",
+                            band.getName() + "-" + band.getMuid());
+                } else {
+                    pathVars.put("pathBand", "_");
+                }
+                pathVars.put("pathRecord", mapping);
+                break;
+
+            case TOUR:
+                pathVars.put("pathTour", mapping);
+                break;
+
+            case TRACK:
+                RecordUrlData record = ((TrackUrlData) entity).getRecord();
+                pathVars.put("pathBand", record.getBand().getName() + "-"
+                        + record.getBand().getMuid());
+                pathVars.put("pathRecord",
+                        record.getName() + "-" + record.getMuid());
+                pathVars.put("pathTrack", mapping);
+                break;
+
+            case USER:
+                UserUrlData user = (UserUrlData) entity;
+                pathVars.put("pathUser",
+                        user.getFirstName() + "-" + user.getLastName() + "-"
+                                + user.getMuid());
+                break;
+
+            case VENUE:
+                pathVars.put("pathVenue", mapping);
+                break;
+
+            default:
+                throw new UnsupportedOperationException("unknown MUID type");
+
+        }
+
+        return pathVars;
+    }
 }
