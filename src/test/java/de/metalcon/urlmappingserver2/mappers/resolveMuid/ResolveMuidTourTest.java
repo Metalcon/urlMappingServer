8e237d91994d8c7b8b8e95effbd20e2e3ac8d4ed
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2014-03-29 21:24:52 +0100

    tour tests created using new mapping scheme

--- src/test/java/de/metalcon/urlmappingserver2/mappers/resolveMuid/ResolveMuidTourTest.java
+++ src/test/java/de/metalcon/urlmappingserver2/mappers/resolveMuid/ResolveMuidTourTest.java
@@ -1,7 +1,5 @@
 package de.metalcon.urlmappingserver2.mappers.resolveMuid;
 
-import java.util.Calendar;
-
 import de.metalcon.domain.MuidType;
 import de.metalcon.testing.MuidFactory;
 import de.metalcon.urlmappingserver.api.requests.registration.EntityUrlData;
@@ -9,8 +7,6 @@ import de.metalcon.urlmappingserver.api.requests.registration.TourUrlData;
 import de.metalcon.urlmappingserver2.mappers.ResolveMuidTest;
 
 public class ResolveMuidTourTest extends ResolveMuidTest {
-
-    protected static final int NO_YEAR = 0;
 
     @Override
     protected MuidType getMuidType() {
@@ -29,17 +25,7 @@ public class ResolveMuidTourTest extends ResolveMuidTest {
     }
 
     public static TourUrlData getTour() {
-        return new TourUrlData(MuidFactory.generateMuid(TYPE), VALID_NAME,
-                getCrrYear());
-    }
-
-    protected static TourUrlData getTourWOYear() {
-        return new TourUrlData(MuidFactory.generateMuid(TYPE), VALID_NAME,
-                NO_YEAR);
-    }
-
-    protected static int getCrrYear() {
-        return Calendar.getInstance().get(Calendar.YEAR);
+        return new TourUrlData(MuidFactory.generateMuid(TYPE));
     }
 
 }
